8be7e19139a5ae816ff921b2bf4e5bbf1d91ed77
sonika agrawal <sonika@saurabhs-mbp.lan>
2020-11-21 13:21:17 -0500

    inital script

--- module/template.tf
+++ module/template.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
     version = "~>2.0"
     features {}
-    subscription_id="a0d9722a-1c0a-4400-baa6-dea5add6b434"
-    client_id = "4486e1f4-0e0f-473a-b399-0a1a88d75e75"
-    client_secret="2LzdzlFuyJ~M-u8Qxc1~Eky6uZDJv6o~J2"
-    tenant_id= "e8efcd8b-e3e7-49a2-b915-351142a365f1"
+    subscription_id="4b37979e-7cbb-4e77-9d44-cf78a1681f4f"
+    client_id = "37fb45ff-a658-4e2e-b3a8-897d48a67899"
+    client_secret="b1e5c65c-bf6b-4fb2-9256-33fd50271847"
+    tenant_id= "560941c2-cd68-484c-892f-0056e1d1947b"
 }
 
 # Create New Resource Group in Azure Subscription
